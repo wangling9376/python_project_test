678d244ae41ed15edbeeff35aec1b78fa67e77a5
wangling9376 <1466621006@qq.com>
2018-10-30 15:50:15 +0800

    web_test_5 "_thread"

--- 2018-10-30/web_test.py
+++ 2018-10-30/web_test.py
@@ -4,36 +4,34 @@ import os,time
 import _thread
 from selenium import webdriver
 
-count = 0
-count_1 = 0
 chromedriver_path = "/usr/local/bin/chromedriver"
 url = 'https://www.baidu.com'
 url_2 = 'https://www.alipay.com/'
-
-
 def open_url(threadName, delay):
-    while (count < 11):
+    count = 0
+    while (count < 5):
         driver = webdriver.Chrome(chromedriver_path)
         # driver.maximize_window() # 最大化浏览器
         driver.implicitly_wait(8)  # 设置隐式时间等待
         driver.get(url)
         driver.find_element_by_name("wd").send_keys("python")
         driver.find_element_by_id("su").click()
-        time.sleep(10)
+        # time.sleep(10)
         # driver.quit()
-
+        count = count + 1
 
 def open_url_2(threadName, delay):
-    while (count_1 < 11):
+    count_1 = 0
+    while (count_1 < 5):
+        count_1 = count_1 + 1
         driver = webdriver.Chrome(chromedriver_path)
         # driver.maximize_window() # 最大化浏览器
         driver.implicitly_wait(8)  # 设置隐式时间等待
-        driver.get(url_2)
-        driver.find_element_by_name("wd").send_keys("python")
+        driver.get(url)
+        driver.find_element_by_name("wd").send_keys("支付宝")
         driver.find_element_by_id("su").click()
-        time.sleep(10)
-        # driver.quit()
-
+        # time.sleep(10)
+        #driver.quit()
 
 try:
     _thread.start_new_thread(open_url, ("Thread-1", 2,))
