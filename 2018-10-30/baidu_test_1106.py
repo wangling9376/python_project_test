3100496514feaffb7d31a824f5319714a136273f
wangling9376 <1466621006@qq.com>
2018-11-10 16:30:50 +0800

    web_test

--- 2018-10-30/baidu_test_1106.py
+++ 2018-10-30/baidu_test_1106.py
@@ -9,9 +9,9 @@ chrome_path = "/usr/local/bin/chromedriver"
 url = "https://www.baidu.com"
 url_1 = "http://test.gs-robot.com/#/analysis"
 #from macos chrome
-#deiver = webdriver.Chrome(chrome_path)
+deiver = webdriver.Chrome(chrome_path)
 # chrome start of windows
-deiver = webdriver.Chrome()
+# deiver = webdriver.Chrome()
 deiver.implicitly_wait(8)
 deiver.get(url_1)
 #deiver.find_element_by_name("wd").send_keys("python")
@@ -19,5 +19,6 @@ deiver.find_element_by_id("email").send_keys("zy@gs-robot.com")
 deiver.find_element_by_id("password").send_keys("1")
 #deiver.find_element_by_id("su").click()
 deiver.find_element_by_css_selector("[type='submit']").click()
-# time.sleep(5)
-# deiver.quit()
+# deiver.find_element_by_link_text("登录").click()
+time.sleep(10)
+deiver.quit()
